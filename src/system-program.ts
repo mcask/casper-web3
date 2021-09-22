cf90e801d62f33cd340f58c25a34bd3f4d9e2554
SiriusWhi <dprripontipo480@gmail.com>
2021-09-22 17:11:10 +0200

    SystemInstruction, SystemProgram classes

---
+++ src/system-program.ts
@@ -0,0 +1,13 @@
+/**
+ * System Instruction class
+ */
+export class SystemInstruction {
+
+}
+
+/**
+ * Factory class for transactions to interact with the System program
+ */
+export class SystemProgram {
+
+}
