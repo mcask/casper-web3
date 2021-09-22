e67199b1d9fee9c00d3bbe08fad3ee66792789b0
dragonheaven <happyfamilylife11@gmail.com>
2021-09-22 19:11:39 +0200

    Add AccountMeta

--- src/transaction.ts
+++ src/transaction.ts
@@ -1,7 +1,21 @@
+import {PublicKey} from './publickey';
+
 /**
  * Transaction signature as base-58 encoded string
  */
 export type TransactionSignature = string;
+
+/**
+ * Account metadata used to define instructions
+ */
+export type AccountMeta = {
+  /** An account's public key */
+  pubkey: PublicKey;
+  /** True if an instruction requires a transaction signature matching `pubkey` */
+  isSigner: boolean;
+  /** True if the `pubkey` can be loaded as a read-write account. */
+  isWritable: boolean;
+};
 
 /**
  * Transaction Instruction class
