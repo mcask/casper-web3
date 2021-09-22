787e7a1a01501a2550ed16eb87d7d0bf2b6fbce3
SiriusWhi <dprripontipo480@gmail.com>
2021-09-22 19:10:08 +0200

    Add Commitment

--- src/connection.ts
+++ src/connection.ts
@@ -1,4 +1,22 @@
 /**
+ * The level of commitment desired when querying state
+ * <pre>
+ *   'processed': Query the most recent block which has reached 1 confirmation by the connected node
+ *   'confirmed': Query the most recent block which has reached 1 confirmation by the cluster
+ *   'finalized': Query the most recent block which has been finalized by the cluster
+ * </pre>
+ */
+export type Commitment =
+  | 'processed'
+  | 'confirmed'
+  | 'finalized'
+  | 'recent' // Deprecated as of v1.5.5
+  | 'single' // Deprecated as of v1.5.5
+  | 'singleGossip' // Deprecated as of v1.5.5
+  | 'root' // Deprecated as of v1.5.5
+  | 'max'; // Deprecated as of v1.5.5
+
+/**
  * A connection to a fullnode JSON RPC endpoint
  */
 export class Connection {
